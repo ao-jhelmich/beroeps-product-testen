d49d93883fe315e931d0d375f40cceb2a75a4d02
Jasper Helmich <jasper.helmich@gmail.com>
2020-12-11 18:37:47 +0100

    wip

---
+++ Stap1/Shop/Shop.Tests/UnitTest1.cs
@@ -0,0 +1,68 @@
+using NUnit.Framework;
+
+namespace Shop.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void when_no_calculation_is_needed_return_0()
+        {
+            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
+
+            double result = determineShippingCosts.ShippingCosts(false, "", 1.1);
+            
+            Assert.AreEqual(0, result);
+        }
+        
+        [Test]
+        public void when_total_price_is_higher_then_1500_return_0()
+        {
+            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
+
+            double result = determineShippingCosts.ShippingCosts(true, "", 1500);
+
+            Assert.AreEqual(0, result);
+        }
+        
+        
+        [Test]
+        public void when_shipping_type_is_ground()
+        {
+            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
+
+            double result = determineShippingCosts.ShippingCosts(true, "Ground", 1);
+
+            Assert.AreEqual(100, result);
+        } 
+        
+        [Test]
+        public void when_shipping_type_is_in_store()
+        {
+            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
+
+            double result = determineShippingCosts.ShippingCosts(true, "InStore", 1);
+
+            Assert.AreEqual(50, result);
+        } 
+        
+        [Test]
+        public void when_shipping_type_is_next_day_air()
+        {
+            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
+
+            double result = determineShippingCosts.ShippingCosts(true, "NextDayAir", 1);
+
+            Assert.AreEqual(250, result);
+        }  
+        
+        [Test]
+        public void when_shipping_type_is_second_day_air()
+        {
+            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
+
+            double result = determineShippingCosts.ShippingCosts(true, "SecondDayAir", 1);
+
+            Assert.AreEqual(125, result);
+        }
+    }
+}
