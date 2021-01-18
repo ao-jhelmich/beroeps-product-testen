79167a68937f284dbdde32e458c33b6273ad80ce
Jasper Helmich <jasper.helmich@gmail.com>
2021-01-18 22:14:43 +0100

    wip

--- Shop/Shop.Tests/UnitTest1.cs
+++ Shop/Shop.Tests/UnitTest1.cs
@@ -4,11 +4,17 @@ namespace Shop.Tests
 {
     public class Tests
     {
+        public DetermineShippingCosts determineShippingCosts;
+
+        [SetUp]
+        public void Setup()
+        {
+            determineShippingCosts = new DetermineShippingCosts();
+        }
+
         [Test]
         public void when_no_calculation_is_needed_return_0()
         {
-            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
-
             double result = determineShippingCosts.ShippingCosts(false, "", 1.1);
             
             Assert.AreEqual(0, result);
@@ -17,8 +23,6 @@ namespace Shop.Tests
         [Test]
         public void when_total_price_is_higher_then_1500_return_0()
         {
-            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
-
             double result = determineShippingCosts.ShippingCosts(true, "", 1500);
 
             Assert.AreEqual(0, result);
@@ -28,8 +32,6 @@ namespace Shop.Tests
         [Test]
         public void when_shipping_type_is_ground()
         {
-            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
-
             double result = determineShippingCosts.ShippingCosts(true, "Ground", 1);
 
             Assert.AreEqual(100, result);
@@ -38,8 +40,6 @@ namespace Shop.Tests
         [Test]
         public void when_shipping_type_is_in_store()
         {
-            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
-
             double result = determineShippingCosts.ShippingCosts(true, "InStore", 1);
 
             Assert.AreEqual(50, result);
@@ -48,8 +48,6 @@ namespace Shop.Tests
         [Test]
         public void when_shipping_type_is_next_day_air()
         {
-            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
-
             double result = determineShippingCosts.ShippingCosts(true, "NextDayAir", 1);
 
             Assert.AreEqual(250, result);
@@ -58,8 +56,6 @@ namespace Shop.Tests
         [Test]
         public void when_shipping_type_is_second_day_air()
         {
-            DetermineShippingCosts determineShippingCosts = new DetermineShippingCosts();
-
             double result = determineShippingCosts.ShippingCosts(true, "SecondDayAir", 1);
 
             Assert.AreEqual(125, result);
